61a39e89627b7b610c58a62b0159749c756020d5
Your Name <vasavilakka398@gmail.com>
2024-10-21 18:00:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from app.models import *
 
+'''
 def insertTopic(request):
     
     if request.method=='POST':
@@ -54,4 +55,57 @@ def insertAccess(request):
         return HttpResponse(f'{n}, {a}, {d}, {e} is created')
     
 
-    return render(request, 'insertAccess.html')
+    return render(request, 'insertAccess.html')'''
+
+
+def insertTopic(request):
+    
+    if request.method=='POST':
+        #return HttpResponse(request.POST['tn'])
+    
+        tn=request.POST['tn']
+        TO=Topic.objects.get_or_create(topic_name=tn)
+        return HttpResponse(f'{tn} is created')
+    
+
+    return render(request, 'insertTopic.html')
+
+
+def insertWebpage(request):
+    LTO=Topic.objects.all()
+    d={'LTO':LTO}
+    
+    if request.method=='POST':
+        #return HttpResponse(request.POST['tn'])
+    
+        tn=request.POST['tn']
+        TO=Topic.objects.get(topic_name=tn)
+        n=request.POST['n']
+        u=request.POST['u']
+        WO=webpage.objects.get_or_create(topic_name=TO,name=n, url=u)
+        return HttpResponse(f'{tn} is created')
+    
+
+    return render(request, 'insertWebpage.html', d)
+
+
+def insertAccess(request):
+    LWO=webpage.objects.all()
+    d={'LWO':LWO}
+    
+    if request.method=='POST':
+        #return HttpResponse(request.POST['tn'])
+    
+        n=request.POST['n']
+        WO=webpage.objects.get(id=n)
+        a=request.POST['a']
+        d=request.POST['d']
+        e=request.POST['e']
+        AO=AccessRecord.objects.get_or_create(name=WO, author=a, date=d,email=e)
+       
+
+
+        return HttpResponse(f'{n}, {a}, {d}, {e} is created')
+    
+
+    return render(request, 'insertAccess.html', d)
